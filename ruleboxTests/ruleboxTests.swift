3ebe4ae0e0ea107265c3382db541071ba6584dd7
KIM HYUN JIN <hyunjin@koriai.com>
2025-05-29 21:31:04 +0900

    프로젝트 구성

---
+++ ruleboxTests/ruleboxTests.swift
@@ -0,0 +1,17 @@
+//
+//  ruleboxTests.swift
+//  ruleboxTests
+//
+//  Created by Ken on 5/29/25.
+//
+
+import Testing
+@testable import rulebox
+
+struct ruleboxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
