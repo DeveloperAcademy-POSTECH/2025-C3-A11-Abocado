1db420311eb9ea940c9ccb2b3521276bb9df0022
freedobby77 <y09urt77@gmail.com>
2025-06-04 17:47:04 +0900

    UI-footer 작업ing

--- rulebox/Views/Rule Book/Sub Rule Modal View.swift
+++ rulebox/Views/Rule Book/Sub Rule Modal View.swift
@@ -35,84 +35,109 @@ struct SubRuleModalView: View {
 
     var body: some View {
         NavigationStack {
-            VStack(spacing: 60) {
-                Button(action: {}) {
-                    Text("이미지가 들어갈 거에요")
-                }
-                HStack {
-                    Text("상세설명 제목").font(.title).bold()
-                    Spacer()
-
-                    // 북마크on상태일때
-                    Button(
-                        action: {
-                            //북마크 해제
-                            if isBookmarkedState {
-                                //북마크 제거
-                                if let bookmark = bookmarks.first(where: {
-                                    $0.content?.id == content.id
-                                }) {
-                                    context.delete(bookmark)
-                                }
-                            } else {
-                                let newBookmark = Bookmark(content: content)
-                                context.insert(newBookmark)
-
-                                showToast = true
-                                DispatchQueue.main.asyncAfter(
-                                    deadline: .now() + 2
-                                ) {
-                                    showToast = false
-                                }
+            
+            ZStack {
+                VStack(alignment: .leading)/*(spacing: 60)*/ {
+                    
+                    VStack(alignment: .leading) {
+                        Text("MajorCat Name").font(.lgRegular)
+                        HStack{
+                            Text("상세설명 제목").font(.mdHeading)
+                            Spacer()
+                            // 북마크
+                            Button(
+                                action: {
+                                    //북마크 해제
+                                    if isBookmarkedState {
+                                        //북마크 제거
+                                        if let bookmark = bookmarks.first(where: {
+                                            $0.content?.id == content.id
+                                        }) {
+                                            context.delete(bookmark)
+                                        }
+                                    } else {
+                                        let newBookmark = Bookmark(content: content)
+                                        context.insert(newBookmark)
+                                        
+                                        showToast = true
+                                        DispatchQueue.main.asyncAfter(
+                                            deadline: .now() + 2
+                                        ) {
+                                            showToast = false
+                                        }
+                                    }
+                                    //상태토글
+                                    isBookmarkedState.toggle()
+                                },
+                                label: {
+                                    // 북마크아이콘
+                                    (isBookmarkedState
+                                        ? AnyView(bookmarkFilledIcon)
+                                        : AnyView(bookmarkIcon))
+                                        .frame(width: 40, height: 40)
+                                })
+                            .onAppear {
+                                isBookmarkedState = isBookmarked
                             }
-
-                            //상태토글
-                            isBookmarkedState.toggle()
-
-                        },
-                        label: {
-                            // 북마크아이콘
-                            (isBookmarkedState
-                                ? AnyView(bookmarkFilledIcon)
-                                : AnyView(bookmarkIcon))
-                                .frame(width: 40, height: 40)
-                        })
-                }
-                .onAppear {
-                    isBookmarkedState = isBookmarked
-                }
-
-                Text(loremIpsum)
-                HStack {
-                    NavigationLink(destination: SearchView()) {
-                        HStack {
-                            Image(systemName: "chevron.left")
-                            Text("이전 페이지")
                         }
                     }
-                    Spacer()
-                    NavigationLink(destination: SearchView()) {
-                        HStack {
-                            Text("다음 페이지")
-                            Image(systemName: "chevron.right")
+                    
+                    HStack {
+                        Spacer()
+                        Text("이미지가 들어갈 거에요")
+                        Spacer()
+                    }
+                    
+                    Text(loremIpsum)
+                }
+                .padding(.horizontal, 20)
+                .overlay(
+                    Group {
+                        if showToast {
+                            Text("북마크에 추가되었습니다")
+                                .padding()
+                                .background(Color.black.opacity(0.7))
+                                .foregroundColor(.white)
+                                .cornerRadius(10)
+                                .transition(.opacity)
+                        }
+                    },
+                    alignment: .bottom
+                )
+                
+                
+                ZStack{
+                    Rectangle()
+                        .frame(maxWidth: .infinity)
+                        .frame(height: 89)
+                        .background(Color.backGround)
+                        .opacity(0.5)
+                        .blur(radius:25)
+                    
+                    HStack {
+                        NavigationLink(destination: SearchView()) {
+                            HStack {
+                                Image(systemName: "chevron.left")
+                                Text("이전 페이지")
+                            }
+                        }
+                        Spacer()
+                        NavigationLink(destination: SearchView()) {
+                            HStack {
+                                Text("다음 페이지")
+                                Image(systemName: "chevron.right")
+                            }
                         }
                     }
+                    
                 }
+                
+            }
+            
+            
+            
+        }//.padding()
 
-            }.overlay(
-                Group {
-                    if showToast {
-                        Text("북마크에 추가되었습니다")
-                            .padding()
-                            .background(Color.black.opacity(0.7))
-                            .foregroundColor(.white)
-                            .cornerRadius(10)
-                            .transition(.opacity)
-                    }
-                },
-                alignment: .bottom
-            )
-        }.padding()
     }
 }
 
