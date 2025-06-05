9f34777f824bcea514c4d7ada5d51679a19532dd
xaxaa <62248038+deerg01@users.noreply.github.com>
2025-06-05 19:35:34 +0900

    connect half modal views

--- rulebox/Views/Components/Main Rule Filter.swift
+++ rulebox/Views/Components/Main Rule Filter.swift
@@ -6,3 +6,40 @@
 //
 
 import Foundation
+import SwiftUI
+
+// TODO: 디자인 컴포넌트 적용 해주세요 ㅠㅅㅠ
+struct ExtensionFilterView: View {
+    var body: some View {
+        VStack {
+            VStack(alignment: .center, spacing: 2) {
+                Text("게임 버전")
+                Text("기본판은 필수 선택입니다")
+            }
+            .padding(.horizontal, 0)
+            .padding(.top, 0)
+            .padding(.bottom, 12)
+            .frame(maxWidth: .infinity, alignment: .top)
+
+        }
+        Text("Extension Filter")
+    }
+}
+
+struct PartyFilterView: View {
+    var body: some View {
+        VStack {
+            HStack {
+                VStack(alignment: .center, spacing: 2) {
+                    Text("게임 인원")
+                    Text("플레이 인원을 선택해주세요")
+                }
+                .padding(.horizontal, 0)
+                .padding(.top, 0)
+                .padding(.bottom, 12)
+                .frame(maxWidth: .infinity, alignment: .top)
+            }
+        }
+        Text("Party Filter")
+    }
+}
--- rulebox/Views/Components/Rule Filter Capsule.swift
+++ rulebox/Views/Components/Rule Filter Capsule.swift
@@ -11,68 +11,68 @@ import SwiftUI
 struct FilterSection: View {
     var filterTags: [FilterTag]
     @ObservedObject var vm: MainRuleBookVM
-    
+
     @State private var showEFModal = false
     @State private var showPFModal = false
 
     var body: some View {
         HStack {
-            Button{
+            Button {
                 showEFModal = true
             } label: {
                 EFCapsule(count: vm.selectedExtensions.count)
             }
             .sheet(isPresented: $showEFModal) {
-                TypoTestView()
+                ExtensionFilterView()
                     .presentationDetents([.medium, .large])
             }
-            
-            Button{
+
+            Button {
                 showPFModal = true
             } label: {
                 PFCapsule(selectedValue: vm.selectedParty ?? "선택되지 않음")
             }
             .sheet(isPresented: $showPFModal) {
-                TypoTestView()
+                PartyFilterView()
                     .presentationDetents([.medium, .large])
             }
-//            DisclosureGroup("확장 추가: \(vm.selectedExtensions.count)개") {
-//                ForEach(extensionValues, id: \.self) { value in
-//                    Button(action: { vm.toggleExtension(value) }) {
-//                        HStack {
-//                            Text(value)
-//                            if vm.selectedExtensions.contains(value) {
-//                                Image(systemName: "checkmark")
-//                            }
-//                        }
-//                    }
-//                }
-//            }
-//            .background(
-//                Color.atomicOpacity20
-//            )
-//            .clipShape(
-//                RoundedRectangle(cornerRadius: 10)
-//            )
-//
-//            DisclosureGroup("인원수 필터") {
-//                ForEach(partyValues, id: \.self) { value in
-//                    Button(action: { vm.selectedParty = value }) {
-//                        HStack {
-//                            Text(value)
-//                            if vm.selectedParty == value {
-//                                Image(systemName: "checkmark")
-//                            }
-//                        }
-//
-//                    }
-//                }
-//            }.background(
-//                Color.atomicOpacity20
-//            )
-//            .clipShape(
-//                RoundedRectangle(cornerRadius: 10)
-//            )
+            //            DisclosureGroup("확장 추가: \(vm.selectedExtensions.count)개") {
+            //                ForEach(extensionValues, id: \.self) { value in
+            //                    Button(action: { vm.toggleExtension(value) }) {
+            //                        HStack {
+            //                            Text(value)
+            //                            if vm.selectedExtensions.contains(value) {
+            //                                Image(systemName: "checkmark")
+            //                            }
+            //                        }
+            //                    }
+            //                }
+            //            }
+            //            .background(
+            //                Color.atomicOpacity20
+            //            )
+            //            .clipShape(
+            //                RoundedRectangle(cornerRadius: 10)
+            //            )
+            //
+            //            DisclosureGroup("인원수 필터") {
+            //                ForEach(partyValues, id: \.self) { value in
+            //                    Button(action: { vm.selectedParty = value }) {
+            //                        HStack {
+            //                            Text(value)
+            //                            if vm.selectedParty == value {
+            //                                Image(systemName: "checkmark")
+            //                            }
+            //                        }
+            //
+            //                    }
+            //                }
+            //            }.background(
+            //                Color.atomicOpacity20
+            //            )
+            //            .clipShape(
+            //                RoundedRectangle(cornerRadius: 10)
+            //            )
         }
     }
 }
@@ -80,28 +80,27 @@ struct FilterSection: View {
 /// Extension Filter Capsule
 struct EFCapsule: View {
     var count: Int
-    
+
     var body: some View {
         HStack(alignment: .center, spacing: 4) {
             Text("확장 추가")
-              .font(
-                Font.custom("Wanted Sans", size: 14)
-                  .weight(.medium)
-              )
-              .foregroundColor(.white)
-            
+                .font(
+                    Font.custom("Wanted Sans", size: 14)
+                        .weight(.medium)
+                )
+                .foregroundColor(.white)
+
             Text("\(count)개")
-              .font(
-                Font.custom("Wanted Sans", size: 14)
-                  .weight(.medium)
-              )
-              .foregroundColor(.red)
-            
+                .font(
+                    Font.custom("Wanted Sans", size: 14)
+                        .weight(.medium)
+                )
+                .foregroundColor(.red)
+
             Image("caret.down")
-              .frame(width: 24, height: 24)
-            
-            
-             }
+                .frame(width: 24, height: 24)
+
+        }
         .padding(.leading, 12)
         .padding(.trailing, 6)
         .padding(.vertical, 6)
@@ -114,22 +113,21 @@ struct EFCapsule: View {
 /// Party Filter Capsule
 struct PFCapsule: View {
     var selectedValue: String
-    
-    var body: some View {
-            HStack(alignment: .center, spacing: 4) {
-                Text("\(selectedValue)명")
-                    .font(Font.custom("Wanted Sans", size: 14).weight(.medium))
-                    .foregroundColor(.white)
-                
-                Image("caret.down")
-                    .frame(width: 24, height: 24)
-            }
-            .padding(.leading, 12)
-            .padding(.trailing, 6)
-            .padding(.vertical, 6)
-            .frame(height: 36)
-            .background(.white.opacity(0.2))
-            .cornerRadius(10)
-        }
-}
 
+    var body: some View {
+        HStack(alignment: .center, spacing: 4) {
+            Text("\(selectedValue)명")
+                .font(Font.custom("Wanted Sans", size: 14).weight(.medium))
+                .foregroundColor(.white)
+
+            Image("caret.down")
+                .frame(width: 24, height: 24)
+        }
+        .padding(.leading, 12)
+        .padding(.trailing, 6)
+        .padding(.vertical, 6)
+        .frame(height: 36)
+        .background(.white.opacity(0.2))
+        .cornerRadius(10)
+    }
+}
