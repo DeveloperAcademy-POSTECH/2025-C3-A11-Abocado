c972b081b4472955361bece5cafd1598523ba466
freedobby77 <y09urt77@gmail.com>
2025-06-06 16:18:24 +0900

    SubRuleModalView() 임시이미지

--- rulebox/Views/Rule Book/Sub Rule Modal View.swift
+++ rulebox/Views/Rule Book/Sub Rule Modal View.swift
@@ -5,18 +5,18 @@ struct SubRuleModalView: View {
     @Environment(\.modelContext) var context
 
     @Query var bookmarks: [Bookmark]
-    
+
     @State private var showToast = false
     @State private var isBookmarked = false
 
     var content: Content
     /* var allContents: [Content]  // 전체 콘텐츠 배열
-    
+
     // 현재 인덱스 계산
     var currentIndex: Int? {
         allContents.firstIndex(where: { $0.id == content.id })
     }
-    
+
     var previousContent: Content? {
         guard let index = currentIndex, index > 0 else { return nil }
         return allContents[index - 1]
@@ -31,28 +31,32 @@ struct SubRuleModalView: View {
         NavigationStack {
             ScrollView {
                 VStack(alignment: .leading) {
-                    
+
                     VStack(alignment: .leading) {
                         Text(content.majorCat.name)
                             .font(.lgRegular)
                             .foregroundColor(.grayNeutral70)
-                        
+
                         HStack {
                             Text(content.name)
                                 .font(.mdHeading)
                                 .foregroundColor(.white)
                             Spacer()
-                            
+
                             // 북마크 버튼
                             Button(action: {
                                 if isBookmarked {
                                     // 북마크 제거
-                                    if let bookmark = bookmarks.first(where: { $0.content?.id == content.id }) {
+                                    if let bookmark = bookmarks.first(where: {
+                                        $0.content?.id == content.id
+                                    }) {
                                         context.delete(bookmark)
                                         try? context.save()
                                     }
                                     showToast = true
-                                    DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
+                                    DispatchQueue.main.asyncAfter(
+                                        deadline: .now() + 2
+                                    ) {
                                         showToast = false
                                     }
                                 } else {
@@ -65,9 +69,9 @@ struct SubRuleModalView: View {
                                 isBookmarked.toggle()
                             }) {
                                 (isBookmarked
-                                 ? AnyView(bookmarkFilledIcon)
-                                 : AnyView(bookmarkIcon))
-                                .frame(width: 40, height: 40)
+                                    ? AnyView(bookmarkFilledIcon)
+                                    : AnyView(bookmarkIcon))
+                                    .frame(width: 40, height: 40)
                             }
                         }
                     }
@@ -75,20 +79,22 @@ struct SubRuleModalView: View {
                     ForEach(0..<content.texts.count, id: \.self) { index in
                         VStack(alignment: .leading) {
                             HStack {
-//                                if let imageData = content.images?[index],
-//                                               let uiImage = UIImage(data: imageData) {
-//                                                Image(uiImage: uiImage)
-//                                                    .resizable()
-//                                                    .scaledToFit()
-//                                                    .frame(height: 200)
-//                                                    .clipped()
-//                                            } else {
-//                                                Color.gray
-//                                                    .frame(height: 200)
-//                                            }
-                                Spacer()
-                                Text("이미지들어가야함")
-                                Spacer()
+                                if let imageData = content.images?[index],
+                                    let uiImage = UIImage(data: imageData)
+                                {
+                                    Spacer()
+                                    Image(uiImage: uiImage)
+                                        .resizable()
+                                        .scaledToFit()
+                                        .frame(height: 200)
+                                        .clipped()
+                                    Spacer()
+                                } else {
+                                    Spacer()
+                                    Color.gray
+                                        .frame(height: 200)
+                                    Spacer()
+                                }
                             }
                             .padding(.bottom, 12)
 
@@ -116,65 +122,69 @@ struct SubRuleModalView: View {
                 )
             }
             .onAppear {
-                isBookmarked = bookmarks.contains { $0.content?.id == content.id }
+                isBookmarked = bookmarks.contains {
+                    $0.content?.id == content.id
+                }
             }
             .onChange(of: bookmarks) { newValue in
-                isBookmarked = newValue.contains { $0.content?.id == content.id }
+                isBookmarked = newValue.contains {
+                    $0.content?.id == content.id
+                }
             }
-            
+
             // 하단 페이지이동버튼 주석
-//            .safeAreaInset(edge: .bottom) {
-//                
-//                HStack {
-//                    NavigationLink(destination: SubRuleModalView(content: content)) {
-//                        HStack {
-//                            Image("caret.left").tint(.white)
-//                            Text("이전페이지").font(.lgRegular)
-//                        }
-//                    }
-//                    Spacer()
-//                    NavigationLink(destination: SubRuleModalView(content: content)) {
-//                        HStack {
-//                            Text("다음페이지").font(.lgRegular)
-//                            Image("caret.left").rotationEffect(.degrees(180)).tint(.white)
-//                        }
-//                    }
-//                }
-//                .padding(.horizontal, 10)
-//                .padding(.top, 10)
-//                .padding(.bottom, 40)
-//                .frame(width: 393, alignment: .trailing)
-//                .background(Color.backGround.opacity(0.5).blur(radius: 25))
-//                
-//                /* if previousContent != nil || nextContent != nil {
-//                    HStack {
-//                        if let previous = previousContent {
-//                            NavigationLink(destination: SubRuleModalView(content: previous, allContents: allContents)) {
-//                                HStack {
-//                                    Image("caret.left").tint(.white)
-//                                    Text(previous.name).font(.lgRegular)
-//                                }
-//                            }
-//                        }
-//                        Spacer()
-//                        if let next = nextContent {
-//                            NavigationLink(destination: SubRuleModalView(content: next, allContents: allContents)) {
-//                                HStack {
-//                                    Text(next.name).font(.lgRegular)
-//                                    Image("caret.left").rotationEffect(.degrees(180)).tint(.white)
-//                                }
-//                            }
-//                        }
-//                    }
-//                    .padding(.horizontal, 10)
-//                    .padding(.top, 10)
-//                    .padding(.bottom, 40)
-//                    .frame(width: 393, alignment: .trailing)
-//                    .background(Color.backGround.opacity(0.5).blur(radius: 25))
-//                } */
-//                
-//            }
-            
+            //            .safeAreaInset(edge: .bottom) {
+            //
+            //                HStack {
+            //                    NavigationLink(destination: SubRuleModalView(content: content)) {
+            //                        HStack {
+            //                            Image("caret.left").tint(.white)
+            //                            Text("이전페이지").font(.lgRegular)
+            //                        }
+            //                    }
+            //                    Spacer()
+            //                    NavigationLink(destination: SubRuleModalView(content: content)) {
+            //                        HStack {
+            //                            Text("다음페이지").font(.lgRegular)
+            //                            Image("caret.left").rotationEffect(.degrees(180)).tint(.white)
+            //                        }
+            //                    }
+            //                }
+            //                .padding(.horizontal, 10)
+            //                .padding(.top, 10)
+            //                .padding(.bottom, 40)
+            //                .frame(width: 393, alignment: .trailing)
+            //                .background(Color.backGround.opacity(0.5).blur(radius: 25))
+            //
+            //                /* if previousContent != nil || nextContent != nil {
+            //                    HStack {
+            //                        if let previous = previousContent {
+            //                            NavigationLink(destination: SubRuleModalView(content: previous, allContents: allContents)) {
+            //                                HStack {
+            //                                    Image("caret.left").tint(.white)
+            //                                    Text(previous.name).font(.lgRegular)
+            //                                }
+            //                            }
+            //                        }
+            //                        Spacer()
+            //                        if let next = nextContent {
+            //                            NavigationLink(destination: SubRuleModalView(content: next, allContents: allContents)) {
+            //                                HStack {
+            //                                    Text(next.name).font(.lgRegular)
+            //                                    Image("caret.left").rotationEffect(.degrees(180)).tint(.white)
+            //                                }
+            //                            }
+            //                        }
+            //                    }
+            //                    .padding(.horizontal, 10)
+            //                    .padding(.top, 10)
+            //                    .padding(.bottom, 40)
+            //                    .frame(width: 393, alignment: .trailing)
+            //                    .background(Color.backGround.opacity(0.5).blur(radius: 25))
+            //                } */
+            //
+            //            }
+
         }
     }
 }
