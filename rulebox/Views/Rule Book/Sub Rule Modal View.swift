e544ebd2033144eef942b400543ff556554eda9f
freedobby77 <y09urt77@gmail.com>
2025-06-10 23:03:04 +0900

    majorCat 페이지에 모든 해당 content 불러오기

--- rulebox/Views/Rule Book/Sub Rule Modal View.swift
+++ rulebox/Views/Rule Book/Sub Rule Modal View.swift
@@ -1,42 +1,32 @@
 import SwiftData
 import SwiftUI
+import Lottie
 
 struct SubRuleModalView: View {
     @Environment(\.modelContext) var context
 
     @Query var bookmarks: [Bookmark]
+    @Query var contents: [Content]
 
     @State private var showToast = false
     @State private var isBookmarked = false
+    //@State private var bookmarkStates: [UUID: Bool] = [:]
 
     var content: Content
-    /* var allContents: [Content]  // 전체 콘텐츠 배열
-
-    // 현재 인덱스 계산
-    var currentIndex: Int? {
-        allContents.firstIndex(where: { $0.id == content.id })
-    }
-
-    var previousContent: Content? {
-        guard let index = currentIndex, index > 0 else { return nil }
-        return allContents[index - 1]
-    }
-
-    var nextContent: Content? {
-        guard let index = currentIndex, index < allContents.count - 1 else { return nil }
-        return allContents[index + 1]
-    } */
 
     var body: some View {
         NavigationStack {
             ScrollView {
                 VStack(alignment: .leading) {
+                    
 
-                    VStack(alignment: .leading) {
                         Text(content.majorCat.name)
                             .font(.lgRegular)
                             .foregroundColor(.grayNeutral70)
-
+                            .multilineTextAlignment(.leading)
+                    
+                    // 한 majorCat 페이지에 모든 content 불러오는건 성공
+                    ForEach(contents.filter{$0.majorCat.id == content.majorCat.id}) {currentContent in
                         HStack {
                             Text(content.name)
                                 .font(.mdHeading)
@@ -74,15 +64,24 @@ struct SubRuleModalView: View {
                                     .frame(width: 40, height: 40)
                             }
                         }
-                    }
-
-            
+                    
+//                        ForEach(0..<content.texts.count, id: \.self) { index in
+//                            ForEach(0..<content.images.count, id: \.self) { index in
+//                                LottieView(content.images[index])
+//                                    .frame(width: 353, height: 275)
+//                                Text(content.texts[index])
+//                                    .foregroundColor(.grayNeutral99)
+//                                    .font(.lgRegular)
+//                                    .padding(.bottom, 34)
+//                            }
+//                        }
+                    
                     ForEach(0..<content.texts.count, id: \.self) { index in
                         let parts = content.texts[index].split(separator: "*", maxSplits: 2, omittingEmptySubsequences: false)
                         let separated = content.texts[index].components(separatedBy: "***")
                         let imageFileName = separated.first ?? ""
                         let textContent = separated.count > 1 ? separated[1] : content.texts[index]
-
+                        
                         VStack(alignment: .leading) {
                             if !imageFileName.isEmpty {
                                 LottieView(filename: imageFileName)
@@ -90,12 +89,25 @@ struct SubRuleModalView: View {
                                     .clipped()
                                         .fixedSize()
                             }
-                            Text(textContent)
+                            Text(content.texts[index])
                                 .foregroundColor(.grayNeutral99)
                                 .font(.lgRegular)
                                 .padding(.bottom, 34)
                         }
                     }
+                    
+//                    ForEach(0..<content.texts.count, id: \.self) { index in
+//                        LottieView(filename: content.images[index])
+//                        Text(content.texts[index])
+//                            .foregroundColor(.grayNeutral99)
+//                            .font(.lgRegular)
+//                            .padding(.bottom, 34)
+//                    }
+                        
+                    }
+                        
+                    
+                        
                 }
                 .padding(.horizontal, 20)
                 .padding(.top, 50)
